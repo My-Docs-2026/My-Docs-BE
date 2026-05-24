f87d2dfee179208d15f5a40097f6b4588332832d
KeonuKim98 <maxman0107@naver.com>
2026-05-24 12:59:34 +0900

    feat: docker 워크플로우 수정 - git 머지 후 docker 이미지 파일 생성 - 파일 생성 후 자동 push

---
+++ My-Docs-BE/src/main/kotlin/com/example/mydocsbe/MyDocsBeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.mydocsbe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MyDocsBeApplication
+
+fun main(args: Array<String>) {
+    runApplication<MyDocsBeApplication>(*args)
+}
