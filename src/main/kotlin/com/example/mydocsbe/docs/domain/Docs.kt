cb3a10bc17fe2cb1d88e7c40e128fb9f11a73b4b
KeonuKim98 <maxman0107@naver.com>
2026-06-12 23:04:10 +0900

    feat: 문서 파일용 필드 추가 - DB에 파일 이름, 사이즈, 형식, key 추가

--- src/main/kotlin/com/example/mydocsbe/docs/domain/Docs.kt
+++ src/main/kotlin/com/example/mydocsbe/docs/domain/Docs.kt
@@ -19,6 +19,14 @@ class Docs(
     val type: DocumentType,
     @Column(name = "file_url")
     val fileUrl: String? = null,
+    @Column(name = "file_name")
+    val fileName: String? = null,
+    @Column(name = "file_key")
+    val fileKey: String? = null,
+    @Column(name = "file_size")
+    val fileSize: Long? = null,
+    @Column(name = "mime_type")
+    val mimeType: String? = null,
     @Column(name = "raw_text", columnDefinition = "TEXT")
     val rawText: String? = null,
     @Enumerated(EnumType.STRING)
