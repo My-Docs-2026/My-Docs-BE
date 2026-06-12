46e010891b45bd04572fd468471d0886e26ae1de
KeonuKim98 <maxman0107@naver.com>
2026-06-12 22:54:50 +0900

    feat: 문서 업로드 contorller, service 추가 - title 조건 구현(없는 경우 자동 생성) - 문서 Id uuid로 구현 - text, file 둘 중 하나로 받도록 명시

---
+++ src/main/kotlin/com/example/mydocsbe/docs/controller/UploadDocsController.kt
@@ -0,0 +1,8 @@
+package com.example.mydocsbe.docs.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("api/v1/upload")
+class UploadDocsController
